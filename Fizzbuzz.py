602beb23cd1f2ae109f2031f87d430e5d55d1d0e
FMaklaryJ <42065595+FMaklaryJ@users.noreply.github.com>
2019-06-09 22:13:16 +0200

    Update Fizzbuzz.py

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -15,20 +15,22 @@
 #What precautions somebody takes when programming reflects the way they think.
 
 
-import time
+import time #Libraries
 import numpy as np
 
-j=0
-numlist=np.array([3,5])
-stringlist=np.array(['Fizz','Buzz'])
+j=0 #Initial condition for counting
+
+numlist=np.array([3,5]) #list of numbers to be replaced with words
+stringlist=np.array(['Fizz','Buzz']) #list of words
 while 1==1:
     j+=1
-    string=str(j)
-    string2=''
-    for i in range(numlist.size):
-        if j % numlist[i] == 0:
-            string2+=stringlist[i]
-            string=string2
+    string=str(j) #Default string to be printed if no number is to be replaced
+    string2='' #String to be manipulated if number should be replaced
     
-    print(string+'\n')
-    time.sleep(0.5)
+    for i in range(numlist.size): 
+        if j % numlist[i] == 0:#Checks modulus of number
+            string2+=stringlist[i] #Appends word to string2
+            string=string2 #Sets string to be printed
+    
+    print(string+'\n') #Prints string
+    time.sleep(0.5) #Waits for 0.5 seconds
